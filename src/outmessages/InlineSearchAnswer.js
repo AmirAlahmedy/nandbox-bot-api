43c894620cb728934b9ef9225d1e0d70718792dd
amir <ameer.alahmedy@gmail.com>
2020-08-19 14:55:39 +0200

    synced outmessages

--- src/outmessages/InlineSearchAnswer.js
+++ src/outmessages/InlineSearchAnswer.js
@@ -17,8 +17,8 @@ module.exports = class InlineSearchAnswer extends OutMessage{
             obj.results = resultArrayObj;
         }
 
-        if (this.searchId) obj.search_id = searchId;
-        if (this.nextOffset) obj.next_offset = nextOffset;
+        if (this.search_id) obj.search_id = search_id;
+        if (this.next_offset) obj.next_offset = next_offset;
 
         return obj;
     }
