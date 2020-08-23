130fee783c826b13fbabeec2abbd0cae07c530d1
amir <ameer.alahmedy@gmail.com>
2020-08-23 12:17:45 +0200

    last commit

--- src/data/Result.js
+++ src/data/Result.js
@@ -18,15 +18,15 @@ module.exports = class Result {
     toJsonObject(){
         let obj;
 
-        if (this.id) obj.id =  id;
-        if (this.caption) obj.caption =  caption;
-        if (this.title) obj.title =  title;
-        if (this.description) obj.description =  description;
-        if (this.url) obj.url =  url;
-        if (this.type) obj.type =  type;
-        if (this.thumbUrl) obj.thumbUrl =  thumbUrl;
-        if (this.width) obj.width =  width;
-        if (this.height) obj.height = height;
+        if (this.id) obj.id =  this.id;
+        if (this.caption) obj.caption =  this.caption;
+        if (this.title) obj.title =  this.title;
+        if (this.description) obj.description =  this.description;
+        if (this.url) obj.url =  this.url;
+        if (this.type) obj.type =  this.type;
+        if (this.thumb_url) obj.thumb_url =  this.thumb_url;
+        if (this.width) obj.width =  this.width;
+        if (this.height) obj.height = this.height;
         
         return obj;
     }
